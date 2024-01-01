b324d7c3277e4712e5f418bc89c019120cee5e75
Grant Jarvis <gmj3@rice.edu>
2023-12-31 23:41:03 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,33 +1,33 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BodyTracking",
-    platforms: [.iOS("15.0")],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "BodyTracking", targets: ["BodyTracking"]),
         .library(name: "FaceTracking", targets: ["FaceTracking"]),
         .library(name: "HandTracking", targets: ["HandTracking"]),
     ],
     dependencies: [
-        .package(name: "RKUtilities", url: "https://github.com/Reality-Dev/RealityKit-Utilities", from: "1.0.0"),
-        .package(url: "https://github.com/Reality-Dev/RealityMorpher", from: "2.0.0"),
+        .package(url: "https://github.com/Reality-Dev/RealityKit-Utilities", from: "1.0.0"),
+        .package(url: "https://github.com/Reality-Dev/RealityMorpher", exact: "2.0.11"),
     ],
     targets: [
         .target(name: "BodyTracking",
                 dependencies: [.target(name: "BTShared"),
-                               .product(name: "RKUtilities", package: "RKUtilities")]),
+                               .product(name: "RKUtilities", package: "RealityKit-Utilities")]),
         .target(name: "FaceTracking",
                 dependencies: [.target(name: "BTShared"),
                                .product(name: "RealityMorpher", package: "RealityMorpher"),
-                               .product(name: "RKUtilities", package: "RKUtilities")]),
+                               .product(name: "RKUtilities", package: "RealityKit-Utilities")]),
         .target(name: "HandTracking",
                 dependencies: [.target(name: "BTShared"),
-                               .product(name: "RKUtilities", package: "RKUtilities")]),
+                               .product(name: "RKUtilities", package: "RealityKit-Utilities")]),
         .target(name: "BTShared",
-                dependencies: [.product(name: "RKUtilities", package: "RKUtilities")]),
+                dependencies: [.product(name: "RKUtilities", package: "RealityKit-Utilities")]),
     ],
     swiftLanguageVersions: [.v5]
 )
