6acc3944caea426f2c5680b42eef2cd36ff8d48d
Grant Jarvis <gmj3@rice.edu>
2024-01-07 00:26:07 -0600

    Make alert optional for ARFaceTrackingConfiguration error.

--- Sources/FaceTracking/FaceTrackingConfig.swift
+++ Sources/FaceTracking/FaceTrackingConfig.swift
@@ -16,11 +16,13 @@ public extension ARView {
      "Because face tracking provides your app with personal facial information, your app must include a privacy policy describing to users how you intend to use face tracking and face data. For details, see the Apple Developer Program License Agreement."
      */
 
-    func runFaceTrackingConfig() throws {
+    func runFaceTrackingConfig(autoAlert: Bool = false) throws {
         // If the iOS device doesn't support face tracking, raise an error.
         guard ARFaceTrackingConfiguration.isSupported
         else {
-            showAlert(title: "Uh oh...", message: "This device does Not support face tracking.")
+            if autoAlert {
+                showAlert(title: "Uh oh...", message: "This device does Not support face tracking.")
+            }
             let errorMessage = "This device does Not support face tracking. This feature is only supported on devices with an A12 chip."
             print(errorMessage)
             throw BodyTrackingError.unsupportedConfiguration("ARFaceTrackingConfiguration is unavailable.")
